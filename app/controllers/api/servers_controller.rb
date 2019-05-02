caa252d3bed71e5568a783aee66a7868151f6eee
timothyjao <timothy.i.jao@gmail.com>
2019-05-02 11:17:04 -0700

    help

--- app/controllers/api/servers_controller.rb
+++ app/controllers/api/servers_controller.rb
@@ -1,25 +1,4 @@
 class Api::UsersController < ApplicationController
 
-    before_action :require_logged_in
-
-    def index
-        @servers = Server.find(user_id)
-
-    def create
-        @server = Server.new(server_params)
-
-        if @user.save
-            login(@user)
-            render "api/servers/show"
-        else
-            render json: @user.errors.full_messages, status: 422
-        end
-    end
-
-    private
-
-    def server_params
-        params.require(:server).permit(:name, :owned_id)
-    end
 end
         
---
+++ app/models/user_server.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+
+    validates :user_id, :server_id, presence: true
+
+    belongs_to :user,
+    foreign_key: :user_id,
+    class_name: :User
+
+    belongs_to :server,
+    foreign_key: :user_servers,
+    source: :Server
+    
+end
