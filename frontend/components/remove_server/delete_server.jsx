5a12f34518e9c066a49a37e01ec6c5d20be76d8d
timothyjao <timothy.i.jao@gmail.com>
2019-06-24 17:25:37 -0700

    refactor delete/leave

--- frontend/components/remove_server/delete_server.jsx
+++ frontend/components/remove_server/delete_server.jsx
@@ -3,96 +3,94 @@ import { withRouter, Link } from "react-router-dom";
 
 class deleteServerForm extends React.Component {
     
-    fullDeleteServer(serverId){
-        this.props.deleteServer(serverId).then(() => this.props.history.push('/browse'))
+    constructor(props) {
+        super(props);
+        this.serverId = parseInt(props.location.pathname.split('/')[2])
+    }
+
+    deleteLeave(header, message, deleteLeaveFunction, button){
+        return (
+            <>
+                <div className="delete-leave">
+                    <div className="delete-leave-header">
+                        <h3 className="header-print">{header}</h3>
+                    </div>
+                    <div className="delete-leave-message">
+                        <p className="message-print">{message}</p>
+                    </div>
+
+                    <div className="delete-leave-selection">
+                        <div className="dl-cancel" onClick={() => this.props.closeModal()}>
+                            <p>Cancel</p>
+                        </div>
+                        <div className="dl-dl" onClick={deleteLeaveFunction}>
+                            <p>{button}</p>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    fullDeleteServer() {
+        this.props.deleteServer(this.serverId).then(() => this.props.history.push('/browse'))
         this.props.closeModal();
     }
 
     delete(){
-        let serverId = parseInt(this.props.location.pathname.split('/')[2])
-        return(
-            <>  
-                <div className="delete-leave">
-                    <div className="delete-leave-header">
-                        <h3 className="header-print">DELETE '{this.props.servers[serverId].name.toUpperCase()}'</h3>
-                    </div>
-                    <div className="delete-leave-message">
-                        <p className="message-print">Are you sure you want to delete <span> </span>
-                            <span className="dl-server-name">{this.props.servers[serverId].name.toUpperCase()}</span>
+        let header = <>DELETE '{this.props.servers[this.serverId].name.toUpperCase()}'</>
+        let message =   <>Are you sure you want to delete <span> </span>
+                            <span className="dl-server-name">{this.props.servers[this.serverId].name.toUpperCase()}</span>
                             ? You will lose all the channels and messages you have created.
-                    </p>
-                    </div>
+                        </>
+        let deleteFunction = () => this.fullDeleteServer(this.serverId)
+        let button = <>Delete Server</>
 
-                    <div className="delete-leave-selection">
-                        <div className="dl-cancel" onClick={() => this.props.closeModal()}>
-                            <p>Cancel</p>
-                        </div>
-                        <div className="dl-dl" onClick={() => this.fullDeleteServer(serverId)}>
-                            <p>Delete Server</p>
-                        </div>
-                    </div>
-                </div>
-            </>
+        return(
+            this.deleteLeave(header, message, deleteFunction, button)
         )
     }
 
-    findUserServer(serverId){
+    findUserServer(){
         let findUserServer;
         Object.values(this.props.usersServers).forEach( userServer => {
-            if(userServer.server_id === serverId)
-                findUserServer = userServer;
+            if(userServer.server_id === this.serverId) findUserServer = userServer;
         })
         return findUserServer
     }
 
-    fullLeaveServer(serverId) {
-        
-        this.props.leaveServer(this.findUserServer(serverId)).then(() => this.props.history.push('/browse'))
+    fullLeaveServer() {
+        this.props.leaveServer(this.findUserServer(this.serverId)).then(() => this.props.history.push('/browse'))
         this.props.closeModal();
     }
 
     leave() {
-        let serverId = parseInt(this.props.location.pathname.split('/')[2])
-        return (
-            <>
-                <div className="delete-leave">
-                    <div className="delete-leave-header">
-                        <h3 className="header-print"> LEAVE '{this.props.servers[serverId].name.toUpperCase()}'</h3>
-                    </div>
-                    <div className="delete-leave-message">
-                        <p className="message-print">Are you sure you want to leave <span> </span>
-                            <span className="dl-server-name">{this.props.servers[serverId].name.toUpperCase()}</span>
+        let header = <>LEAVE '{this.props.servers[this.serverId].name.toUpperCase()}'</>
+        let message = <>Are you sure you want to leave <span> </span>
+                            <span className="dl-server-name">{this.props.servers[this.serverId].name.toUpperCase()}</span>
                             ? You won't be able to rejoin this server unless you are re-invited.
-                    </p>
-                    </div>
+                      </>
+        let deleteFunction = () => this.fullLeaveServer(this.serverId)
+        let button = <>Leave Server</>
 
-                    <div className="delete-leave-selection">
-                        <div className="dl-cancel" onClick={() => this.props.closeModal()}>
-                            <p>Cancel</p>
-                        </div>
-                        <div className="dl-dl" onClick={() => this.fullLeaveServer(serverId)}>
-                            <p>Leave Server</p>
-                        </div>
-                    </div>
-                </div>
-            </>
+        return (
+            this.deleteLeave(header, message, deleteFunction, button)
         )
     }
 
     invite() {
-        let serverId = parseInt(this.props.location.pathname.split('/')[2])
         return (
             <>
                 <div className="delete-leave">
                     <div className="invite-header">
-                        <h3 className="header-print">INVITE FRIENDS TO '{this.props.servers[serverId].name.toUpperCase()}'</h3>
+                        <h3 className="header-print">INVITE FRIENDS TO '{this.props.servers[this.serverId].name.toUpperCase()}'</h3>
                     </div>
                     <div className="invite-message">
                         <p className="message-print"> Share this link with others to grant access to this server </p>
                     </div>
 
                     <div className="invite-selection">
-                        <p>https://dunder.com/{this.props.servers[serverId].url}</p>
+                        <p>https://dunder.com/{this.props.servers[this.serverId].url}</p>
                     </div>
                 </div>
             </>
